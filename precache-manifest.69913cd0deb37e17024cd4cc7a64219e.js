01b0104bda3ac3e08d06efced24757c4924d2c0a
crehds <carlos15erh@gmail.com>
2018-11-28 00:26:48 -0500

    Updates

---
+++ precache-manifest.69913cd0deb37e17024cd4cc7a64219e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "95e7e13bc61843fd00d8e4b5eef58691",
+    "url": "/bachatealo/static/media/4.95e7e13b.jpg"
+  },
+  {
+    "revision": "2e6391588d54c849beda",
+    "url": "/bachatealo/static/css/main.9b2048f4.chunk.css"
+  },
+  {
+    "revision": "63ba697a99ad2a00ac54",
+    "url": "/bachatealo/static/js/1.63ba697a.chunk.js"
+  },
+  {
+    "revision": "e4287331d59858531c4c",
+    "url": "/bachatealo/static/js/runtime~main.e4287331.js"
+  },
+  {
+    "revision": "2d57c362b12259886be6fb1232b032db",
+    "url": "/bachatealo/static/media/logo.2d57c362.svg"
+  },
+  {
+    "revision": "beb651e25a01105613f8b89e0f2647e0",
+    "url": "/bachatealo/static/media/1.beb651e2.jpg"
+  },
+  {
+    "revision": "270eaa9769b333d4cda1c7730f90ad02",
+    "url": "/bachatealo/static/media/2.270eaa97.jpg"
+  },
+  {
+    "revision": "01a233d297b619803c14fb4ec1cfa110",
+    "url": "/bachatealo/static/media/3.01a233d2.jpg"
+  },
+  {
+    "revision": "2e6391588d54c849beda",
+    "url": "/bachatealo/static/js/main.2e639158.chunk.js"
+  },
+  {
+    "revision": "52b40d7fad900152ff0a7aa47be78d86",
+    "url": "/bachatealo/static/media/5.52b40d7f.jpg"
+  },
+  {
+    "revision": "a8cd8f0df282dca88b44bdef33bd37da",
+    "url": "/bachatealo/static/media/6.a8cd8f0d.jpg"
+  },
+  {
+    "revision": "7efbf46400c5111b60b2f2cd92be3763",
+    "url": "/bachatealo/static/media/7.7efbf464.jpg"
+  },
+  {
+    "revision": "9dad0d00d52266014fd90969aaa3caca",
+    "url": "/bachatealo/static/media/8.9dad0d00.jpg"
+  },
+  {
+    "revision": "5509f38f9c148203c45b35c3a2f6f2ea",
+    "url": "/bachatealo/static/media/9.5509f38f.jpg"
+  },
+  {
+    "revision": "7f839fb56b6521c111b1b036056e83bb",
+    "url": "/bachatealo/static/media/practica.7f839fb5.jpg"
+  },
+  {
+    "revision": "a45b02e055ad0c5d9982be7ebba1a050",
+    "url": "/bachatealo/index.html"
+  }
+];
