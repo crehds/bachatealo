e5d7cfcba2a7aa624740153b5407fd00fe0e9cd2
Unknown <carlos15erh@gmail.com>
2018-11-30 15:24:12 -0500

    Adding postcss-apply and postcss-nesting rules

--- bachatealo/postcss.config.js
+++ bachatealo/postcss.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   plugins: [
+    require('postcss-apply'),
     require('postcss-import'),
     require('postcss-preset-env')({
       autoprefixer: {
         grid: true,
         flexbox: false,
+      },
+      features: {
+        'nesting-rules': true,
       },
       preserve: false,
     }),
