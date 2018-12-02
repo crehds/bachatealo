4e49961206de445d0157a1100fd01a55c2d052f5
Unknown <carlos15erh@gmail.com>
2018-12-01 22:08:48 -0500

    Adding video

--- bachatealo/src/containers/Video.jsx
+++ bachatealo/src/containers/Video.jsx
@@ -7,36 +7,32 @@ class Video extends Component {
       <section id={this.props.id} className="Video">
         <div className="video container">
           <figure>
-            <iframe
-              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbachatealo%2Fvideos%2F303067066926144%2F&show_text=0&width=560"
-              title='video1'
-              style={{
-                border: 'none',
-                overflow: 'hidden',
-                width: 'inherit',
-                height: 'inherit',
-              }}
-              scrolling="no"
-              frameBorder="0"
-              allowtransparency="true"
-              allowFullScreen={true}>
-            </iframe>
+            <div>
+              <iframe
+                src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbachatealo%2Fvideos%2F303067066926144%2F&show_text=0&width=560"
+                title='video1'
+                width= '560'
+                height='315'
+                scrolling="no"
+                frameBorder="0"
+                allowtransparency="true"
+                allowFullScreen={true}>
+              </iframe>
+            </div>
           </figure>
           <figure>
-            <iframe
-              src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbachatealo%2Fvideos%2F225816234651228%2F&show_text=0&width=560"
-              title='video2'
-              style={{
-                border: 'none',
-                overflow: 'hidden',
-                width: 'inherit',
-                height: 260,
-              }}
-              scrolling="no"
-              frameBorder="0"
-              allowtransparency="true"
-              allowFullScreen={true}>
-            </iframe>
+            <div>
+              <iframe
+                src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbachatealo%2Fvideos%2F225816234651228%2F&show_text=0&width=560"
+                title='video2'
+                width= '560'
+                height='315'
+                scrolling="no"
+                frameBorder="0"
+                allowtransparency="true"
+                allowFullScreen={true}>
+              </iframe>
+            </div>
           </figure>
         </div>
       </section>
