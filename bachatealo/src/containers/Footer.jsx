2b53ec3be0527003da6caeaddab6277eee521987
Unknown <carlos15erh@gmail.com>
2018-12-06 20:46:27 -0500

    Data and structure of footer

---
+++ bachatealo/src/containers/Footer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps() {
+  return {
+
+  };
+}
+
+class Footer extends Component {
+  render () {
+    return {
+
+    };
+  }
+}
+
+export default connect(mapStateToProps)(Footer);
