96a6dd02353c189db31fd2c22bb94b02899d8a91
Unknown <carlos15erh@gmail.com>
2018-12-22 01:58:15 -0500

    Añadiendo datos normalizados

--- bachatealo/src/entries/Bachatealo.js
+++ bachatealo/src/entries/Bachatealo.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react';
 import HandleError from '../containers/Handle-error.jsx';
 import Portada from '../containers/Portada.jsx';
-import Hero from '../containers/Hero.jsx';
-import History from '../containers/History.jsx';
-import Location from '../containers/Location.jsx';
-import Event from '../containers/Eventos.jsx';
-import Fotos from '../containers/Fotos.jsx';
-import Video from '../containers/Video.jsx';
-import Footer from '../containers/Footer.jsx';
+// import Hero from '../containers/Hero.jsx';
+// import History from '../containers/History.jsx';
+// import Location from '../containers/Location.jsx';
+// import Event from '../containers/Eventos.jsx';
+// import Fotos from '../containers/Fotos.jsx';
+// import Video from '../containers/Video.jsx';
+// import Footer from '../containers/Footer.jsx';
+// import Portada from '../components/Portada.jsx';
 import HomeLayout  from '../components/Home-layout.jsx';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state, props) {
+  const results = state.data.sections.map((sectionId) => state.data.entities.section[sectionId]);
+
+  const sections = {
+    portada: {},
+    hero: {},
+    history: {},
+    location: {},
+    event: {},
+    fotos: {},
+    videos: {},
+    footer: {},
+  };
+
+  let cont = 0;
+  for (let i in sections) {
+    sections[i] = results[cont];
+    cont++;
+  }
+
+  return {
+    sections,
+  };
+}
 
 class Bachatealo extends Component {
   render() {
     return (
       <HandleError>
         <HomeLayout>
-          <Portada/>
-          <Hero/>
+        { console.log(this.props.sections)}
+          <Portada portada={this.props.sections.portada}/>
+          {/*<Hero />
           <History/>
           <Location/>
           <Event/>
           <Fotos/>
           <Video/>
-          <Footer/>
+          <Footer/>*/}
         </HomeLayout>
       </HandleError>
     );
   }
 }
 
-export default Bachatealo;
+export default connect(mapStateToProps)(Bachatealo);
