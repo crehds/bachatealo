c909271403248a15f08530d302912b9443b80637
Unknown <carlos15erh@gmail.com>
2021-03-26 20:31:47 -0500

    adding animation for photos on Hero

--- bachatealo/src/components/Hero.jsx
+++ bachatealo/src/components/Hero.jsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Section from './Section.jsx';
 import Media from './Media.jsx';
 
-function Hero(props) {
-  return (
-    <Section
-      id={props.id}
-      class={props.class}
-    >
-    {/*Contenedor de las fotos iniciales*/}
-    <div className="hero-container">
-      {
-        props.album.map((item) =>
-          <Media
-            {...item}
-            key={item.id}
-          />
-        )
-      }
-    </div>
-    </Section>
-  );
+export default class Hero extends Component {
+  addAnimation = (elements) => {
+    console.log(elements);
+    elements.forEach((e, i) =>
+      setTimeout(
+        (e) => {
+          e.style.animationName = 'appear';
+          e.style.animationDuration = '1.5s';
+          e.style.aniamtionIterationCount = '1';
+          e.style.animationTimingFunction = 'cubic-bezier(0.57, 0.07, 0.44, 2)';
+          e.style.opacity = '1';
+        },
+        500 * i,
+        e
+      )
+    );
+  };
+
+  componentDidMount() {
+    let elements = [...document.getElementsByClassName('photo__hero')];
+    this.addAnimation(elements);
+  }
+
+  render() {
+    return (
+      <Section id={this.props.id} class={this.props.class}>
+        {/*Contenedor de las fotos iniciales*/}
+        <div className='hero-container'>
+          {this.props.album.map((item, i) => (
+            <Media {...item} key={item.id} className='photo__hero' />
+          ))}
+        </div>
+      </Section>
+    );
+  }
 }
 
-export default Hero;
+// function Hero(props) {
+//   return (
+//     <Section id={props.id} class={props.class}>
+//       {/*Contenedor de las fotos iniciales*/}
+//       <div className='hero-container'>
+//         {props.album.map((item, i) => (
+//           <Media {...item} key={item.id} />
+//         ))}
+//       </div>
+//     </Section>
+//   );
+// }
+
+// export default Hero;
